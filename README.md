2c2f7a47eb1e52b00b4fd08cab2498484d3a713e
DanielDaCosta <daniel.pereiracosta@hotmail.com>
2022-03-27 14:27:03 -0300

    Deploy to Rinkeby; delete folder app

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# erc721-dapp
+# ERC721 Dapp
+
+# Requirements
+
+```json
+    "@openzeppelin/contracts": "^4.5.0",
+    "@truffle/hdwallet-provider": "^2.0.4",
+    "web3": "^1.7.1",
+    "webpack-dev-server": "^4.7.4"
+```
+
+```
+    Truffle v5.5.3
+    Solidity Compiler:  0.8.12+commit.f00d7308
+```
+
+# ERC721 Token
+```
+    Name: Daniel Token
+    Symbol: DPC
+```
+
+# Deployment to Rinkeby Testnet Network
+
+```
+   Account: 0x507536263bf3e03f1Cd52d06Ff5f863EA4b08d92
+   Contract Token: 0x4884972172f8e7a2880C0a23969B4e545Bbd2ca9
+```
--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,7 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -46,6 +46,12 @@ module.exports = {
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
     // },
+    rinkeby: {
+      provider: () => new HDWalletProvider("YOUR_PRIVATE_KEY", `wss://rinkeby.infura.io/ws/v3/{INFURA_KEY}`),
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    }
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
